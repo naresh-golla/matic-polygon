d54dae2d194fdd28a7b47f11613d1683472192f1
naresh_golla <nareshgolla.529@gmail.com>
2021-03-01 01:23:34 +0530

    logo issue fixed

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -46,21 +46,17 @@ const Request = ()=>{
 const Header = ({ showNav, siteTitle, scrolled, navMenuItems = [], textWhite = true }) => {
 
   let headerClass = "fixed w-full z-30 top-0 text-white";
-  // headerClass += scrolled ? " bg-white shadow" : "";
 
   let navActionClass =
     "mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 text-white";
-  // navActionClass += textWhite || !scrolled ? " bg-white text-gray-800" : "";
-  // navActionClass += textWhite || scrolled ? " gradient text-white" : "";
+
 
   let navContentClass =
     "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20 text-white";
-  // navContentClass += textWhite || !scrolled ? " lg:bg-transparent bg-gray-100" : "";
-  // navContentClass += textWhite || scrolled ? " bg-white" : "";
+
 
   let titleClass = "toggleColour no-underline hover:no-underline font-bold text-2xl lg:text-4xl text-white";
-  // titleClass += textWhite || scrolled ? " text-gray-800" : "";
-  // titleClass += textWhite || !scrolled ? " text-white" : "";
+
 
   return (
     <nav id="header" className={headerClass} style={{background:"#0a1647"}}>
